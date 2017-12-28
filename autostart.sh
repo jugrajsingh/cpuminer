e8d297cc309ef1537d079439160b35828e3c879a
Jugraj Singh <jugraj.singh@paxcom.net>
2017-12-28 23:19:49 +0530

    Autostart Added

---
+++ autostart.sh
@@ -0,0 +1,16 @@
+sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
+
+if  ! [ -e "cpuminer" ]
+then
+	./build.sh
+fi
+
+if test $1 = q;
+then
+	./cpuminer -a qubit -o stratum+tcp://s1.theblocksfactory.com:9000 -u jugraj.worker -p 4
+elif test $1 = m;
+then
+	./cpuminer -a myr-gr -o stratum+tcp://stratum.dgb-groestl.theblocksfactory.com:9003 -u jugraj.worker -p 4
+else
+echo 'Not Specified'
+fi
