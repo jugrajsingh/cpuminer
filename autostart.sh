1236c2f347414a2b0da6558c195a9b64fb066e44
Jugraj Singh <jugraj.singh@paxcom.net>
2018-01-02 22:07:38 +0530

    Autostart Updated

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,4 @@
-sudo apt-get -y install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ screen zlib1g-dev tor
+sudo apt-get -y install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ zlib1g-dev
 
 if  ! [ -e "cpuminer" ]
 then
@@ -15,18 +15,18 @@ if test $1 = DGB;
 then
 	if test $2 = q;
 	then
-		screen torify ./cpuminer -a qubit -o stratum+tcp://s1.theblocksfactory.com:9000 -u jugraj.worker -p 4
+		./cpuminer -a qubit -o stratum+tcp://s1.theblocksfactory.com:9000 -u jugraj.worker -p 4
 	elif test $1 = m;
 	then
-		screen torify ./cpuminer -a myr-gr -o stratum+tcp://stratum.dgb-groestl.theblocksfactory.com:9003 -u jugraj.worker -p 4
+		./cpuminer -a myr-gr -o stratum+tcp://stratum.dgb-groestl.theblocksfactory.com:9003 -u jugraj.worker -p 4
 	fi
 elif test $1 = ETN;
 then
 	if test $3 = easy;
 	then
-		screen torify ./cpuminer -a cryptonight -o stratum+tcp://etn.easyhash.io:3630 -u etnkKPTknoHRSGXHakPfSSaDxaBnbAZ51gbdjsQ7eBN3Ru4AZVd49RLMFJVZDbodUTA7y9cmityG6EBszT1Dr4VR7RK7a71SMj -p 4
+		./cpuminer -a cryptonight -o stratum+tcp://etn.easyhash.io:3630 -u etnkKPTknoHRSGXHakPfSSaDxaBnbAZ51gbdjsQ7eBN3Ru4AZVd49RLMFJVZDbodUTA7y9cmityG6EBszT1Dr4VR7RK7a71SMj -p 4
 	else
-		screen torify ./cpuminer -a cryptonight -o stratum+tcp://asiapool.electroneum.com:3333 -u etnkKPTknoHRSGXHakPfSSaDxaBnbAZ51gbdjsQ7eBN3Ru4AZVd49RLMFJVZDbodUTA7y9cmityG6EBszT1Dr4VR7RK7a71SMj -p 4
+		./cpuminer -a cryptonight -o stratum+tcp://asiapool.electroneum.com:3333 -u etnkKPTknoHRSGXHakPfSSaDxaBnbAZ51gbdjsQ7eBN3Ru4AZVd49RLMFJVZDbodUTA7y9cmityG6EBszT1Dr4VR7RK7a71SMj -p 4
 	fi
 else
 echo 'Currency Not Specified'
